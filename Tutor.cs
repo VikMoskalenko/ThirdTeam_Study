8e4c1e658357ecd8571de76c7a19e83db0d6a9dd
Volodymyr Russkikh <ebriuspythagoras@gmail.com>
2024-05-22 21:14:29 +0300

    Add properties + ctors

--- Tutor.cs
+++ Tutor.cs
@@ -3,8 +3,28 @@ namespace ThirdTeam_Study
 {
     public class Tutor
     {
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public const string DescriptionLink = "https//hillel.com/tutordesc/11dw2e2e34rw";
+
+        public readonly int ID { get; set; }
+
+        [SetsRequiredMembers]
+        public Tutor(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        [SetsRequiredMembers]
         public Tutor()
         {
+            FirstName = "Unknown";
+            LastName = "Tutor";
         }
     }
 }
