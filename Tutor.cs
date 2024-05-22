e5799edcb02cedae1d89bc26112c65f1c43415dc
Volodymyr Russkikh <ebriuspythagoras@gmail.com>
2024-05-22 22:05:55 +0300

    Already done Tutor class

--- Tutor.cs
+++ Tutor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-
 
 namespace ThirdTeam_Study
 {
@@ -11,7 +9,7 @@ namespace ThirdTeam_Study
 
         public required string LastName { get; set; }
 
-        public DateOnly? BDay { get; set; }
+        public DateOnly? BDay { get; }
 
         public const string DescriptionLink = "https//hillel.com/tutordesc/11dw2e2e34rw";
 
@@ -26,9 +24,19 @@ namespace ThirdTeam_Study
             LastName = lastname;
             BDay = bday;
             ID = (this.FirstName + this.LastName + this.BDay.ToString()).GetHashCode(); //самая примитивная реализация для примера, в идеале это
-            Age = DateTime.Now.Year-bday.Year;
+            Age = DateTime.Now.Year - bday.Year;
         }
-    
+
+        public string ToTeach ()
+        {
+            return "I will teach you today!";
+        }
+
+        public string ToTeach(int lesson_id)
+        {
+            return "I will teach you today on the lesson: "+lesson_id.ToString()+"!";
+        }
     }
+
 }
 
