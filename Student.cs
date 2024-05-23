8c66ffe977b25fa49b303a152ff6c00b28d03f8e
Viktoria Moskalenko <vicky1234555617@gmail.com>
2024-05-23 20:16:12 +0100

    initial commit

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Student
+{
+	public string Id { get; }
+	public required string Name { get; set; }
+	public required string LastName { get; set; }
+	public const string University = "Hogwarts";
+	public DateOnly? DOB { get; set; }
+	public readonly string StudyYear;
+	public Student (string id, string studyyear, string name, string lastname)
+	{
+		Id = id;
+		StudyYear = studyyear;
+		Name = name;
+		LastName = lastname;
+
+	}
+	public string GetStudentInfo()
+	{
+		return $"Id : {Id}, name : {Name}, lastName {LastName} ";
+	}
+
+	public string StudentSayHello()
+	{
+		return $"Hello, I am {Name}";
+	}
+}
