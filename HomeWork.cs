1526ecbb7901b47ecd529c5aa181794e9dd2c1f2
VadymVyshnevskiy <vadym.vyshnevskiy@skyresponse.com>
2024-05-26 17:18:23 +0300

    Added HomeWork Class

---
+++ HomeWork.cs
@@ -0,0 +1,65 @@
+﻿namespace ThirdTeam_Study
+{
+    public class HomeWork
+    {
+        private readonly Tutor Tutor;
+
+        const string noFile = "File not found";
+        // ToDo: chenge to lesson type
+        public ushort HomeWorkNumber { get; set; } = 1;
+        // ToDo: redo to Student type
+        public string StudentName { get; set; } = "Mike Tyson";
+        public required string Id { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public ushort Grade { get; set; }
+
+        protected string HomeWorkFile { get; set; } = string.Empty;
+
+        private static readonly string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        private readonly string destinationDirectory = Path.Combine(basePath.Replace("bin\\Debug\\net8.0\\", ""), "uploads");
+
+        public HomeWork(Tutor _tutor)
+        {
+            Tutor = _tutor;
+        }
+
+        public void UploadHomeWork(string sourceFilePath)
+        {
+            if (!File.Exists(sourceFilePath))
+            {
+                throw new FileNotFoundException(noFile, sourceFilePath);
+            }
+
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+            string fileName = Path.GetFileName(sourceFilePath);
+
+            string destinationFilePath = Path.Combine(destinationDirectory, fileName);
+
+            File.Copy(sourceFilePath, destinationFilePath, overwrite: true);
+        }
+
+        public void GradeHomeWork(ushort grade) {
+            while (true)
+            {
+                if (HomeWorkFile.Length == 0)
+                {
+                    Console.WriteLine("Please upload home work first");
+                    break;
+                }
+                if ( grade < 1 || grade > 100) {
+                    Console.WriteLine("incorrect rating range Enter from 1 to 100");
+                } else
+                {
+                    Grade = grade;
+                    Console.WriteLine($"Homework is worth {grade} points ");
+                    break;
+                }
+            }
+        }
+
+        public string GetTutorFullName() => $"{Tutor.FirstName} {Tutor.LastName}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,52 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            HomeWork hw1 = new(new Tutor("Jhon", "Dou", new DateOnly(1991, 1, 24))) 
+            { 
+                Id= Guid.NewGuid().ToString() 
+            };
+
+            while (true)
+            {
+                Console.WriteLine("1. Choose file");
+                Console.WriteLine("2. Exit");
+                Console.Write("Choose option: ");
+                string option = Console.ReadLine() ?? string.Empty; ;
+
+                if (option == "2")
+                {
+                    break;
+                }
+                else if (option == "1")
+                {
+                    Console.WriteLine("Choose path to your home work file:");
+                    string sourceFilePath = Console.ReadLine() ?? string.Empty;
+
+                    try
+                    {
+                        hw1.UploadHomeWork(sourceFilePath ?? string.Empty);
+                        Console.WriteLine("File saved successfully.");
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error occur: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong option. Please try again.");
+                }
+            }
+
+            hw1.GradeHomeWork(100);
+            hw1.Comment = "Looks good to me";
+
+            Console.WriteLine(String.Format("| {0,15 } | {1,20} | {2,15} | {3,25} |", "Teacher", "Home work number", "Grade", "Comment"));
+            Console.WriteLine("| {0,-15} | {1,-20} | {2, -15} | {3, -25} |", new string('-', 15), new string('-', 20), new string('-', 15), new string('-', 25));
+            Console.WriteLine(String.Format("| {0,15} | {1,20} | {2,15} | {3,25} |", hw1.GetTutorFullName(), hw1.HomeWorkNumber, hw1.Grade, hw1.Comment));
         }
     }
 }
