c66eb1f18c68efaab0e08d86548c59c0b5627bc5
Illia Krasheninnikov <krasheninnikovillia@gmail.com>
2024-05-31 15:26:45 +0200

    LessontExtension added

---
+++ LessonExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTeam_Study
+{
+    static class LessontExtension
+    {
+        public static void StudentPresent(this Lesson lesson, string id)
+        {
+            foreach (Student student in lesson.Students)
+            {
+                if (student.Id == id)
+                {
+                    Console.WriteLine($"Student {student.Name} {student.LastName} is on lesson {lesson.LessonType}");
+                    break;
+                }
+            }
+        }
+
+        public static void StudentPresent(this Lesson lesson, string name, string lastName)
+        {
+            foreach (Student student in lesson.Students)
+            {
+                if (student.Name == name && student.LastName == lastName)
+                {
+                    Console.WriteLine($"Student ID: {student.Id} - {student.Name} {student.LastName} is on lesson {lesson.LessonType}");
+                    break;
+                }
+            }
+        }
+    }
+}
