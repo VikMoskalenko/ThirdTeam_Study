39239393a0aff28084877921221ad18edeadb2bf
Viktoria Moskalenko <vicky1234555617@gmail.com>
2024-06-05 22:19:24 +0100

    added Student Manages and changed type of Id to int

---
+++ Managers/StudentManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ThirdTeam_Study.Managers
+{
+    public class StudentManager
+    {
+        public StudentManager() 
+        { 
+
+        }
+        
+        public string GetStudentInfo()
+        {
+            return "Hello, student  ";
+        }
+
+        public string  GetStudentInfo(string Name, string LastName, int Id)
+        {
+            return $"Id: {Id}, name : {Name}, lastName {LastName} ";
+        }
+
+        public string StudentSayHello(string Name)
+        {
+            return $"Hello, I am {Name}";
+        }
+    }
+}
