7f628d6c6db8d7c369a6a4daf406bc2e534ec25c
Volodymyr Russkikh <ebriuspythagoras@gmail.com>
2024-06-05 22:00:34 +0300

    I have done CreateTutor method

--- TutorManager.cs
+++ TutorManager.cs
@@ -6,6 +6,17 @@ namespace ThirdTeam_Study
         public TutorManager()
         {
         }
+
+        public Tutor CreateTutor(Guid id, PersonalInfo info)
+        {
+            return new Tutor
+            {
+                Info = new PersonalInfo { First_name = info.First_name, Last_name = info.Last_name, Birthday = info.Birthday },
+                Id = id,
+                Age = info.Birthday.ToCalculateAge(),
+                Seniority = 0
+            };
+        }
     }
 }
 
