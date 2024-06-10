774e1fa318d51760b984ea1a9a995489db3b430d
Данил Науменко <d.naumenko1111@gmail.com>
2024-06-10 13:43:24 +0200

    Added EdPlatformManager

---
+++ Exceptions/ConnectionError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTeam_Study.Exceptions
+{
+    [Serializable]
+    public class ConnectionError : Exception
+    {
+        public ConnectionError() { }
+        public ConnectionError(string message) : base(message) { }
+    }
+}
