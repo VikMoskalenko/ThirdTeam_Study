3c3e4df00b18c00d727dd4810184b78fa4f94a98
Volodymyr Russkikh <ebriuspythagoras@gmail.com>
2024-06-13 15:07:24 +0300

    Add overloading WriteToFile signature in IFileManager

--- Interfaces/IFileManager.cs
+++ Interfaces/IFileManager.cs
@@ -4,6 +4,7 @@ namespace ThirdTeam_Study.Interfaces
     public interface IFileManager <T>
     {
         public bool WriteToFile(T obj);
+        public bool WriteToFile(List<T>? list);
         public List<T> ReadAllFromFile();
         public bool ClearFile();
         protected string ToParceJSON(string json);
