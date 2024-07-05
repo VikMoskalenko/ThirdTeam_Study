0a64ba3d673b96a32a1e2a8ed3aeffb7aa50980b
Viktoria Moskalenko <vicky1234555617@gmail.com>
2024-07-05 18:09:06 +0100

    added student table to sql code

--- ThirdTeam_Study.Data/SqlScripts/CreateDB.sql
+++ ThirdTeam_Study.Data/SqlScripts/CreateDB.sql
@@ -56,3 +56,21 @@ VALUES (1, 'Olga'),
 
 GO
 
+CREATE TABLE Student (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    LastName NVARCHAR(50) NOT NULL,
+    DOB DATE NULL
+);
+Select  * from Student
+INSERT INTO Student (Name, LastName, DOB) VALUES ('John', 'Doe', '2000-01-01');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Jane', 'Smith', '1999-02-15');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Michael', 'Johnson', '2001-03-10');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Emily', 'Davis', '2002-04-05');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Matthew', 'Brown', '2003-05-20');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Olivia', 'Wilson', '2004-06-25');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Daniel', 'Moore', '2005-07-30');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Sophia', 'Taylor', '2006-08-15');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Alexander', 'Anderson', '2007-09-05');
+INSERT INTO Student (Name, LastName, DOB) VALUES ('Emma', 'Thomas', '2008-10-10');
+Go
